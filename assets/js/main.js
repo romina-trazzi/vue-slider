ae8de3944447db55cd054333a630a9f531a09165
Romina Trazzi <romina.trazzi@posta.it>
2020-12-16 02:43:16 +0100

    Commit iniziale
    File bozza del progetto

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+let app = new Vue ({
+    el: "#root",
+    data: {
+        counter: 0,
+        images: [
+            "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
+            "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg", 
+            "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+            "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
+        ]
+    },
+
+    methods: {
+        nextImg() {
+            this.counter++;
+            if(this.counter === this.images.length) {
+                this.counter = 0;
+            }
+        },
+
+        prevImg()  {
+            if(this.counter > 0) {
+                this.counter--;
+            } else {
+                this.counter = this.images.length -1;
+            }
+        }
+    }
+
+
+
+
+});
